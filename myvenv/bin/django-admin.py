f757a1cea74797ca5adec7ced41dcf4c52805c25
Gaurav Jain <gauravcdj.98@gmail.com>
2018-10-02 22:15:02 +0530

    done for prodiction

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/cuberdude/coding/help/divaynshdsc/Django_Proj/myvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
